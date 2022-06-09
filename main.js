8501e670fe94367c909352daf85e8babf2b35bab
VoitMax <maximilian.voit@student.uibk.ac.at>
2022-06-09 17:42:21 +0200

    Popup Hinzugefügt

--- main.js
+++ main.js
@@ -42,3 +42,18 @@ let miniMap = new L.Control.MiniMap(
 
 // TODO: geojson laden und anzeigen
 
+async function loadBorders(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    // console.log("Geojson borders: ", geojson);
+    L.geoJSON(geojson,{
+        onEachFeature: function(feature, layer){
+            console.log("Feature: ", feature)
+            layer.bindPopup(`<h4>${feature.properties.VIERTEL_NA}</h4>`)
+        },
+    }).addTo(map);
+
+}
+loadBorders("data/viertelgrenzen.geojson");
+
+
